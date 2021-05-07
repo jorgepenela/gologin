c7d5cf0c494b63d483d529410639a4a4da104b81
jorge <jorge.penela@gmail.com>
2021-05-07 14:59:02 -0300

    Add files via upload

---
+++ gologin/gologin.php
@@ -0,0 +1,33 @@
+<?php 
+
+/*
+Plugin Name: Go Login
+Plugin URI: https://github.com/jorgepenela
+Description: Redirecciona a los usuarios no logueados a la pagina de Login. 
+Version: 1.0
+Author: Jorge Penela
+Author URI: https://github.com/jorgepenela
+License: GPL2
+*/
+
+
+
+defined('ABSPATH') or die("Bye bye");
+
+$site_url = network_site_url( '/' );
+// redireccionar a los usuarios no logueados
+function jp_goLogin(){
+	if (!is_user_logged_in()) {
+
+		if ( $GLOBALS['pagenow'] !== 'wp-login.php' ) {
+		    // We're on the login page!
+		    wp_redirect( '/wp-login.php');
+
+		    exit;
+
+		    }
+		}	
+		
+}
+
+add_action('init', 'jp_goLogin');
